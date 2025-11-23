6f0d072500a88adb542cb2eaabe09edd15a30728
Adrian <149493213+AKAu101@users.noreply.github.com>
2025-11-23 18:35:25 +0100

    Added Volume Control for FMOD
    - added Variable that controls the audio volume through a VCA connection
    - VCA needs to be created for it to work

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,4 +1,4 @@
-using System.CodeDom;
+    using System.CodeDom;
 using System.Diagnostics;
 using UnityEngine;
 using FMODUnity;
@@ -12,12 +12,18 @@ public class AudioManager : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] FirstPersonController controller;
     [SerializeField] AtmosphereTransition atmosphere;
-    
+
+    [Header("Volume Control")]
+    [SerializeField] [Range(0f, 1f)] private float masterVolume = 1f;
+
     private EventInstance footstepInstance;
     private EventInstance windInstance;
     private bool wasPlayingLastFrame = false;
     private bool wasGroundedLastFrame = true;
     private bool wasWindyLastFrame = false;
+
+    // VCA for volume control
+    private VCA masterVCA;
     void Start()
     {
         RuntimeManager.LoadBank("Master", true);
@@ -32,6 +38,17 @@ public class AudioManager : MonoBehaviour
         landDesc.loadSampleData();
         metalJumpDesc.loadSampleData();
         metalLandDesc.loadSampleData();
+
+        // Initialize VCA for volume control
+        try
+        {
+            masterVCA = RuntimeManager.GetVCA("vca:/Master");
+            SetMasterVolume(masterVolume);
+        }
+        catch (System.Exception e)
+        {
+            UnityEngine.Debug.LogWarning($"Could not find Master VCA. Make sure you have a VCA called 'Master' in your FMOD project. Error: {e.Message}");
+        }
     }
     void Update()
     {
@@ -103,6 +120,27 @@ public class AudioManager : MonoBehaviour
         }
     }
     
+    /// <summary>
+    /// Set the master volume (0 to 1)
+    /// Can be called from UI sliders or code
+    /// </summary>
+    public void SetMasterVolume(float volume)
+    {
+        masterVolume = Mathf.Clamp01(volume);
+        if (masterVCA.isValid())
+        {
+            masterVCA.setVolume(masterVolume);
+        }
+    }
+
+    /// <summary>
+    /// Get the current master volume (0 to 1)
+    /// </summary>
+    public float GetMasterVolume()
+    {
+        return masterVolume;
+    }
+
     void OnDestroy()
     {
         // Clean up the instance
